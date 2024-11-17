38083f4680be43bacae1a2c6ee4e0f104bb63245
Victor <idalys1357@gmail.com>
2024-11-17 22:49:50 +0300

    Попытки

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,5 +4,5 @@ const isClient = typeof window !== 'undefined';
 
 export const api = axios.create({
   timeout: 3000,
-  baseURL: 'https://onetimelink.ru/api',
+  baseURL: isClient ? '/api' : process.env['SERVER_SIDE_URL'],
 });
