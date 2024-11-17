33c84cb545f886c67df1e9290acecc45dde5b0f9
Victor <idalys1357@gmail.com>
2024-11-17 22:25:18 +0300

    Попытки

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
-import {headers} from 'next/headers';
 
 const isClient = typeof window !== 'undefined';
 
 export const api = axios.create({
   timeout: 3000,
-  baseURL: isClient ? '/api' : process.env.BACKAPI_URL,
-  headers: !isClient ? {'x-real-ip': headers().get('x-real-ip')} : {},
+  baseURL: 'https://onetimelink.ru/',
 });
