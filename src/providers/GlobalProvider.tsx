77472cb644c3f08b0623e873d712b21de3eaa571
Victor <idalys1357@gmail.com>
2024-11-20 20:02:49 +0300

    Убрал трейсер

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,12 +1,13 @@
 import {ModalProvider} from '@/providers/ModalProvider/ModalProvider';
 import {QueryClientProvider} from '@/providers/QueryClient/QueryClient';
 import {SnackbarProvider} from '@/providers/SnackbarProvider/SnackbarProvider';
-import {Tracer} from '@/providers/Tracer/Tracer';
+
+// import {Tracer} from '@/providers/Tracer/Tracer';
 
 export const GlobalProvider = ({children}: {children: React.ReactNode}) => {
   return (
     <QueryClientProvider>
-      <Tracer appToken={`${process.env['TRACER_TOKEN']}`} />
+      {/*<Tracer appToken={`${process.env['TRACER_TOKEN']}`} />*/}
       <SnackbarProvider>
         <ModalProvider>{children}</ModalProvider>
       </SnackbarProvider>
