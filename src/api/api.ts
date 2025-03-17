c08fc36ca2191f94e84eb8672297aa6299326e13
Victor <idalys1357@gmail.com>
2025-03-17 22:04:15 +0300

    Тест впн

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,14 +2,7 @@ import axios from 'axios';
 
 const isClient = typeof window !== 'undefined';
 
-const getClientUrl = () => {
-  if (window.Cypress) {
-    return 'https://onetimelink.ru/api';
-  }
-  return '/api';
-};
-
 export const api = axios.create({
   timeout: 3000,
-  baseURL: isClient ? getClientUrl() : process.env['SERVER_SIDE_URL'],
+  baseURL: isClient ? '/api' : process.env['SERVER_SIDE_URL'],
 });
