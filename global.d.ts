e75b50ae24c54f1a42e10f3f8495cca8c0bc8d3e
Victor <idalys1357@gmail.com>
2025-08-08 20:38:51 +0300

    buildfix

--- global.d.ts
+++ global.d.ts
@@ -4,7 +4,7 @@ type Messages = typeof ru;
 declare global {
   interface Window {
     ym: any;
-    yaContextCb?: Array<any>;
+    yaContextCb?: Array<() => void>;
   }
   interface IntlMessages extends Messages {}
 }
